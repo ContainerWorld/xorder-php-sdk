160a43787813b816ff46184c76aa673182c561c9
Ryan Stratton <me@ryanstratton.ca>
2015-07-11 13:05:11 -0700

    Add xOrder Response class.

---
+++ src/Response.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * xOrder Response.
+ *
+ * @package     container-world/xorder
+ * @author      Ryan Stratton <ryan@craftt.com>
+ * @copyright   Copyright (c) Ryan Stratton
+ * @license     https://github.com/craftt/xorder-php-sdk/blob/master/LICENSE.md Apache 2.0
+ * @link        https://github.com/craftt/xorder-php-sdk
+ */
+
+namespace XOrder;
+
+use SimpleXMLElement;
+use XOrder\Contracts\ResponseInterface;
+
+/**
+ * Session
+ */
+class Response implements ResponseInterface {
+
+    /**
+     * @var \SimpleXMLElement
+     */
+    private $data;
+
+    /**
+     * Constructor
+     *
+     * @param string $xml
+     */
+    public function __construct($xml)
+    {
+        $this->data = new SimpleXMLElement($xml);
+    }
+
+    /**
+     * Property overloader.
+     *
+     * @param  string $value
+     * @throws \ErrorException
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        if ($property === 'data') {
+            return $this->data;
+        }
+
+        if (property_exists($this->data, $property)) {
+            return $this->data->$property;
+        }
+
+        return null;
+    }
+
+    /**
+     * Covert XML response data to array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return json_decode($this->toJson(), true);
+    }
+
+    /**
+     * Convery XML response data to JSON.
+     *
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode($this->data);
+    }
+
+}
---
+++ src/contracts/ResponseInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * xOrder Response Interface.
+ *
+ * @package     container-world/xorder
+ * @author      Ryan Stratton <ryan@craftt.com>
+ * @copyright   Copyright (c) Ryan Stratton
+ * @license     https://github.com/craftt/xorder-php-sdk/blob/master/LICENSE.md Apache 2.0
+ * @link        https://github.com/craftt/xorder-php-sdk
+ */
+
+namespace XOrder\Contracts;
+
+/**
+ * XOrder Interface
+ */
+interface ResponseInterface {
+
+    /**
+     * Covert XML response data to array.
+     *
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * Convery XML response data to JSON.
+     *
+     * @return string
+     */
+    public function toJson();
+ 
+}
