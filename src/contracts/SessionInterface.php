181b1887d0a10c763a8e14c6f1eeb0dc7e92814a
Ryan Stratton <me@ryanstratton.ca>
2015-07-11 13:01:10 -0700

    Fix license link.

--- src/contracts/SessionInterface.php
+++ src/contracts/SessionInterface.php
@@ -6,7 +6,7 @@
  * @package     container-world/xorder
  * @author      Ryan Stratton <ryan@craftt.com>
  * @copyright   Copyright (c) Ryan Stratton
- * @license     http://www.apache.org/licenses/LICENSE-2.0
+ * @license     https://github.com/craftt/xorder-php-sdk/blob/master/LICENSE.md Apache 2.0
  * @link        https://github.com/craftt/xorder-php-sdk
  */
 
